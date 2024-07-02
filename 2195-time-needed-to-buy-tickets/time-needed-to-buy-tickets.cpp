41b00044e30512d0490696079d6071601491ac26
RAHUL TIWARY <91451569+rahult7526@users.noreply.github.com>
2024-07-03 00:06:50 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (60.29%) - LeetSync

--- 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.cpp
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.cpp
@@ -1,22 +1,14 @@
 class Solution {
-public:
-    int timeRequiredToBuy(vector<int>& tickets, int k) {
-        int ans = 0;
-        for (int i = 0; i < tickets.size(); i++) {
-            if (i <= k) {
-                if (tickets[i] <= tickets[k]) {
-                    ans += tickets[i];
-                } else {
-                    ans += tickets[k];
-                }
-            } else {
-                if (tickets[i] < tickets[k]) {
-                    ans += tickets[i];
-                } else {
-                    ans += tickets[k] - 1;
-                }
-            }
-        }
-        return ans;
-    }
+ public:
+  int timeRequiredToBuy(vector<int>& tickets, int k) {
+    int ans = 0;
+
+    for (int i = 0; i < tickets.size(); ++i)
+      if (i <= k)
+        ans += min(tickets[i], tickets[k]);
+      else
+        ans += min(tickets[i], tickets[k] - 1);
+
+    return ans;
+  }
 };
