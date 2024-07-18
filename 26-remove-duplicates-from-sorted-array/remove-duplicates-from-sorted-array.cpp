2eb55ba5261822a7a3774a52012ea82c80384231
RAHUL TIWARY <91451569+rahult7526@users.noreply.github.com>
2024-07-18 22:38:57 +0530

    Time: 11 ms (34.38%) | Memory: 21.4 MB (17.24%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -4,11 +4,11 @@ public:
         int n = nums.size();
         int i = 0;
         for(int j=1; j<n; j++){
-            if(nums[i] != nums[j]){
+            if(nums[j] != nums[i]){
                 nums[i+1] = nums[j];
                 i++;
             }
         }
-        return i+1;
+        return i+1;// size of array 
     }
 };
